9521cdd11f90d60ce9a50a956cb5e30709bfb7a3
andergamer12 <88795772+andergamer12@users.noreply.github.com>
2024-08-11 22:16:54 -0500

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,170 @@
+// auth.js
+import { auth } from './firebase-config.js';
+import { signInWithPopup, GithubAuthProvider, GoogleAuthProvider, FacebookAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const githubButton = document.getElementById('github-login-button');
+    const googleButton = document.getElementById('google-login-button');
+    const facebookButton = document.getElementById('facebook-login-button');
+
+    if (githubButton) githubButton.addEventListener('click', iniciarSesionConGitHub);
+    if (googleButton) googleButton.addEventListener('click', iniciarSesionConGoogle);
+    if (facebookButton) facebookButton.addEventListener('click', iniciarSesionConFacebook);
+});
+
+function iniciarSesionConGitHub() {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log("Inicio de sesión con GitHub exitoso:", user);
+            window.location.href = 'principal.html';
+        }).catch((error) => {
+            const errorMessage = error.message;
+           
+            console.error("Error en el inicio de sesión con GitHub:", errorMessage);
+        });
+}
+
+function iniciarSesionConGoogle() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log("Inicio de sesión con Google exitoso:", user);
+            window.location.href = 'principal.html';
+        }).catch((error) => {
+            const errorMessage = error.message;
+            
+            console.error("Error en el inicio de sesión con Google:", errorMessage);
+        });
+}
+
+function iniciarSesionConFacebook() {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log("Inicio de sesión con Facebook exitoso:", user);
+            window.location.href = 'principal.html';
+        }).catch((error) => {
+            const errorMessage = error.message;
+            
+            console.error("Error en el inicio de sesión con Facebook:", errorMessage);
+        });
+}
+
+function registrar() {
+    const form = document.getElementById('registerForm');
+    
+    // Eliminar cualquier event listener existente
+    form.removeEventListener('submit', handleSubmit);
+    
+    // Añadir el nuevo event listener
+    form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    if (password.length < 6) {
+        alert("La contraseña debe tener al menos 6 dígitos.");
+        return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Registro de usuario exitoso:", user);
+            alert("Usuario registrado satisfactoriamente!");
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("Error en el registro:", errorMessage);
+        });
+}
+
+function ingreso() {
+    const form = document.getElementById('loginForm1');
+    
+    // Eliminar cualquier event listener existente
+    form.removeEventListener('submit', handleLoginSubmit);
+    
+    // Añadir el nuevo event listener
+    form.addEventListener('submit', handleLoginSubmit);
+}
+
+function handleLoginSubmit(e) {
+    e.preventDefault();
+    const email1 = document.getElementById('email1').value;
+    const password1 = document.getElementById('password1').value;
+
+    signInWithEmailAndPassword(auth, email1, password1)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Inicio de sesión exitoso:", user);
+            window.location.href = 'principal.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert("Los datos ingresados son incorrectos");
+            console.error("Error en el inicio de sesión:", errorMessage);
+        });
+}
+
+function recupPassword() {
+    document.getElementById('recupPasswordForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        const email = document.getElementById('email2').value;
+
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                console.log("Correo de recuperación enviado con éxito");
+                alert("Correo enviado satisfactoriamente");
+                window.location.href = 'index.html';
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert("El correo no se ha enviado");
+                console.error("Error al enviar el correo de recuperación:", errorMessage);
+            });
+    });
+}
+
+function nomusu() {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const welcomeMessageDiv = document.querySelector('div');
+            if (welcomeMessageDiv) {
+                welcomeMessageDiv.innerHTML += ` <strong>${user.displayName || user.email}</strong>`;
+            }
+        } else {
+            window.location.href = 'index.html';
+        }
+    });
+}
+
+function cerrarSesion() {
+    signOut(auth)
+        .then(() => {
+            console.log("Sesión cerrada con éxito");
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            console.error("Error al cerrar sesión:", error.message);
+        });
+}
+
+window.cerrarSesion = cerrarSesion;
+window.nomusu = nomusu;
+window.registrar = registrar;
+window.ingreso = ingreso;
+window.recupPassword = recupPassword;
+
+export { iniciarSesionConGitHub, iniciarSesionConGoogle, iniciarSesionConFacebook, cerrarSesion, nomusu, registrar, ingreso, recupPassword };
---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// firebase-config.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAoAJkzDKDXX4qwDfrnfgTJifmt1icHqco",
+    authDomain: "prueba-7825e.firebaseapp.com",
+    projectId: "prueba-7825e",
+    storageBucket: "prueba-7825e.appspot.com",
+    messagingSenderId: "132138237384",
+    appId: "1:132138237384:web:088cda156737e3d0fd14fd"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
+
+
+        
